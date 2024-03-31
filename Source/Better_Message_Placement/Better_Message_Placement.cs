6a0feabc23371353d23f9401123ab7eee28019de
Mlie <emipa606@users.noreply.github.com>
2024-03-31 22:21:43 +0200

    Mod updated for 1.5 and passed autotests

--- Source/Better_Message_Placement/Better_Message_Placement.cs
+++ Source/Better_Message_Placement/Better_Message_Placement.cs
@@ -1,7 +1,6 @@
 ﻿using HarmonyLib;
 using Verse;
 
-<<<<<<< HEAD
 namespace Better_Message_Placement;
 
 [StaticConstructorOnStartup]
@@ -12,18 +11,5 @@ public class Better_Message_Placement
     static Better_Message_Placement()
     {
         harmony.PatchAll();
-=======
-namespace Better_Message_Placement
-{
-    [StaticConstructorOnStartup]
-    public class Better_Message_Placement
-    {
-        private static readonly Harmony harmony = new Harmony("rimworld.dyrewulfe.bettermessageplacement");
-
-        static Better_Message_Placement()
-        {
-            harmony.PatchAll();
-        }
->>>>>>> eb44ffc87f53d393dea8a09952335573473e572e
     }
 }
